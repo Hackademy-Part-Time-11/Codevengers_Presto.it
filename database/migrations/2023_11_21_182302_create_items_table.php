517da7c6316900a2eaa4d4b724accea55a3ca88e
Ale-97 <alessio.piras.1997@gmail.com>
2023-11-21 21:41:40 +0100

    migration base

---
+++ database/migrations/2023_11_21_182302_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 40); // Titolo con massimo 20 caratteri
+            $table->decimal('price', 8, 2); // Prezzo numerico con virgola
+            $table->text('description'); // Descrizione
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
