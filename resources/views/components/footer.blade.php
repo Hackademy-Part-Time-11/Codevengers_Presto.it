d628fd9cd6f09bd1406cfbc5cab63940d582d146
FrancescAdduce <francesco.adduce@outlook.com>
2023-11-22 20:49:59 +0100

    CAPIAMO 2

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,16 @@
+<div>
+    <ul>
+        <li>
+        <a href="">Chi siamo</a>
+        </li>
+        <li>
+            <a href="">Contattaci</a>
+        </li>
+        <li>
+            <a href="">Lavora con noi</a>
+        </li>
+    </ul>
+</div>
 <div class="container-fluid bg-dark text-light py-3">
     Powered by Alessio Piras, Francesco Adduce e Alessandro Ricci
 </div>
