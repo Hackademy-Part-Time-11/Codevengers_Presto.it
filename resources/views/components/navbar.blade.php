e6d214947d7e2a8983aa619f7efad97dd5a4b8fd
alessandro.ricci <riccialessandro2002@gmail.com>
2023-11-22 19:17:42 +0100

    bg-navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,12 +10,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('articles') }}">Articoli</a>
                 </li>
-                <li class="nav-item">
-                    <a  class="nav-link" href="{{ route('chi-sono') }}">Chi sono</a>
-                </li>
-                <li class="nav-item">
-                    <a  class="nav-link" href="{{ route('contatti') }}">Contatti</a>
-                </li>
             </ul>
                 {{-- {{ auth()->user()->email }} --}}
             <ul class="navbar-nav ms-auto mb-2 mb-2 mb-lg-0">
