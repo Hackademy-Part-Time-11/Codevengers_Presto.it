a1547a21732de4d3c34633f57d1c7ac19b968f8d
alessandro.ricci <riccialessandro2002@gmail.com>
2023-11-27 22:04:05 +0100

    item parte 3

--- resources/views/components/item-annuncio.blade.php
+++ resources/views/components/item-annuncio.blade.php
@@ -1,13 +1,15 @@
 <x-layout-main>
 
 
-    <div>
+    <div id="item">
         <div class="card">
-            <div class="card-body py-3 text-center">
-                <h3>
-                    {{$item->title}}
-                </h3>
-                <div class=" col-lg-3">
+            <h3 class="d-flex justify-content-center">
+                {{$item->title}}
+            </h3>
+            <div class="card-body py-3 text-center row">
+                
+                
+                <div class=" col-3 ">
                     <div id="carouselExampleCaptions" class="carousel slide">
                         <div class="carousel-indicators">
                             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -16,20 +18,20 @@
                             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
                         </div>
                         <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="https://images.ctfassets.net/l7x57e7jkz0c/2tDMBA5HmCHVrJ5Q7uMcOq/8eda8946f664ad684736c20866c3292b/5338w3-BH-CCGAS-Web-Header---1400x400---Book-Appointment-WRK.jpg" class="d-block w-100 rounded mx-auto" alt="photo1">
+                            <div class="carousel-item active ">
+                                <img  src="https://picsum.photos/400/600" class="d-block w-100 rounded mx-auto object-fit-contain" alt="photo1">
                             
                             </div>
                             <div class="carousel-item">
-                                <img src="https://www.ecaldwellstyle.com/wp-content/uploads/2019/05/E-Caldwell-Style-Closet-Overhaul-Banner-1400x400.jpg" class="d-block w-100 rounded mx-auto" alt="photo2">
+                                <img src="https://picsum.photos/400/601" class="d-block w-100 rounded mx-auto object-fit-contain" alt="photo2">
                                 
                             </div>
                             <div class="carousel-item">
-                                <img src="https://buildingbiologyinstitute.org/wp-content/uploads/2019/03/products-stores-1400x400.jpg" class="d-block w-100 rounded mx-auto" alt="photo3">
+                                <img src="https://picsum.photos/400/602" class="d-block w-100 rounded mx-auto object-fit-contain" alt="photo3">
                             
                             </div>
                             <div class="carousel-item">
-                                <img src="https://picsum.photos/1400/500" class="d-block w-100 rounded mx-auto" alt="photo3">
+                                <img src="https://picsum.photos/400/603" class="d-block w-100 rounded mx-auto object-fit-contain" alt="photo3">
                             
                             </div>
                         </div>
@@ -43,9 +45,13 @@
                         </button>
                     </div>
                 </div>
-                <div class="m-2 ">
-                    <!-- {{-- descrizione --}} -->
+                
+                <div class="col-9">
+                    <div class="m-2 ">
+                        {{$item->description}}
+                    </div>
                 </div>
+                
             
             
                 <div class="mt-5">
