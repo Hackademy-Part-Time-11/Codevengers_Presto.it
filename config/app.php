be23feabe2dd3f36664eeaa915d8bfa956dc506b
alessandro.ricci <riccialessandro2002@gmail.com>
2023-12-11 18:22:42 +0100

    downlode chat

--- config/app.php
+++ config/app.php
@@ -160,9 +160,9 @@ return [
          * Package Service Providers...
          */       
         
-         App\Providers\FortifyServiceProvider::class,
-         TeamTNT\Scout\TNTSearchScoutServiceProvider::class,
-         Laravel\Scout\ScoutServiceProvider::class,
+            App\Providers\FortifyServiceProvider::class,
+            TeamTNT\Scout\TNTSearchScoutServiceProvider::class,
+            Laravel\Scout\ScoutServiceProvider::class,
         /*
          * Application Service Providers...
          */
