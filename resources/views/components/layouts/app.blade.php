a0fbee79c4b2b0bd9f86601402a7799d4262a927
Ale-97 <alessio.piras.1997@gmail.com>
2023-12-15 19:51:52 +0100

    home

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/x-icon" />
     <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('images/logo.png') }}">
-    <title>{{ $title ?? 'Page Title' }}</title>
+    <title>{{ $title ?? config('app.name')}}</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
 
