c3b23210244d6cbab42baa2b26bd6f46716b38f3
Ale-97 <alessio.piras.1997@gmail.com>
2023-12-15 17:51:00 +0100

    loggin-registerPage 2.0

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,10 +2,10 @@
 
     <div class="container" id="login">
         <div class="row">
-            <div class="col-lg-3 col-md-4 mx-auto mt-5">
+            <div class="col-lg-4 col-md-5 mx-auto mt-5">
                 <div class="card">
                     <span class="d-flex justify-content-end">
-                        <a href="{{ url('auth/google') }}">
+                        <a id="authG" href="{{ url('auth/google') }}">
                             <i class="bi bi-google"></i>
                         </a>
                     </span>
@@ -17,22 +17,22 @@
                             @csrf
                             <div class="m-3 d-flex flex-column">
                                 <label for="email">Email</label>
-                                <input type="email" name="email" id="email" @error('email') is-invalid @enderror class="inputLogin" value="{{old('email')}}">
+                                <input type="email" name="email" id="email" @error('email') is-invalid @enderror class="input" value="{{old('email')}}">
                                 @error('email')
                                 <span class="text-danger small"> {{$message}} </span> @enderror
                             </div>
                             <div class="m-3">
                                 <label for="password">Password</label>
-                                <input type="password" name="password" id="password" @error('password') is-invalid @enderror class="inputLogin">
+                                <input type="password" name="password" id="password" @error('password') is-invalid @enderror class="input">
                                 @error('password')
                                 <span class="text-danger small"> {{$message}} </span> @enderror
                             </div>
                             <div class="mt-5 mb-3 mx-3 d-flex justify-content-center">
-                                <button type="submit" id="accedi">Accedi</button>
+                                <button type="submit" id="confirm">Accedi</button>
                             </div>
                             <div class=" d-flex justify-content-center">
                                 <div>Se non hai un account</div>
-                                <a href="/register">Registrati</a>
+                                <a id="changePage" href="/register">Registrati</a>
                             </div>
 
                         </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,47 +1,56 @@
 <x-layout-main>
 
-    
-    <div class="row">
-        <div class="col-lg-5 mx-auto mt-5">
-            <div class="card">
-                <div class="card-header">
-                    Registrati
+    <div class="container" id="register">
+        <div class="row">
+            <div class="col-lg-4 col-md-5 mx-auto mt-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>Registrati</h1>
+                    </div>
+                    <div class="m-2">
+
+                        <form action="/register" method="POST">
+                            @csrf
+                            <div class="m-3">
+                                <label for="name">Nome</label>
+                                <input clas type="text" name="name" id="name" @error('name') is-invalid @enderror class="input" value="{{old('name')}}">
+                                @error('name')
+                                <span class="text-danger small"> {{$message}} </span> @enderror
+                            </div>
+                            <div class="m-3">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" id="email" @error('email') is-invalid @enderror class="input" value="{{old('email')}}">
+                                @error('email')
+                                <span class="text-danger small"> {{$message}} </span> @enderror
+                            </div>
+                            <div class="m-3">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" id="password" @error('password') is-invalid @enderror class="input">
+                                @error('password')
+                                <span class="text-danger small"> {{$message}} </span> @enderror
+                            </div>
+                            <div class="m-3">
+                                <label for="password_confirmation">Conferma Password</label>
+                                <input type="password" name="password_confirmation" id="password_confirmation" class="input">
+
+                            </div>
+                            <div class="mt-5 mb-3 d-flex justify-content-center">
+                                <button type="submit" id="confirm">Registrati</button>
+                            </div>
+                            <div class=" d-flex justify-content-center">
+                                <div>Se non hai un account</div>
+                                <a id="changePage" href="/login">Accedi</a>
+                            </div>
+
+                        </form>
+
+                    </div>
+
                 </div>
-                <div class="m-2">
-                    <form action="/register" method="POST">
-                        @csrf
-                        <div class="m-3">
-                            <label for="name">Nome</label>
-                            <input type="text" name="name" id="name" @error('name') is-invalid @enderror class="form-control" value= "{{old('name')}}">
-                            @error('name')
-                            <span class="text-danger small"> {{$message}} </span> @enderror
-                        </div>
-                        <div class="m-3">
-                            <label for="email">Email</label>
-                            <input type="email" name="email" id="email" @error('email') is-invalid @enderror class="form-control" value= "{{old('email')}}">
-                            @error('email')
-                            <span class="text-danger small"> {{$message}} </span> @enderror
-                        </div>
-                        <div class="m-3">
-                            <label for="password">Password</label>
-                            <input type="password" name="password" id="password" @error('password') is-invalid @enderror class="form-control">
-                            @error('password')
-                            <span class="text-danger small"> {{$message}} </span> @enderror
-                        </div>
-                        <div class="m-3">
-                            <label for="password_confirmation">Conferma Password</label>
-                            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
-                            
-                        </div>
-                        <div class="mt-5 mb-3 mx-3">
-                            <button type="submit">Registrati</button>
-                        </div>
-                    </form>
-                </div>
-                
+
             </div>
-            
         </div>
     </div>
+
 
 </x-layout-main>
